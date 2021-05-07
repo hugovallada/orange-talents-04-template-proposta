ee73c292e31e8c74898332d39fb8722f7af63c2f
hugovallada <valladahugo@gmail.com>
2021-05-07 12:26:09 -0300

    Adiciona o actuator e libera o endpoint de health

--- src/main/java/com/br/zupacademy/hugo/proposta/proposta/consulta/ConsultaPropostaClient.java
+++ src/main/java/com/br/zupacademy/hugo/proposta/proposta/consulta/ConsultaPropostaClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "ConsultaProposta", url = "${consulta.solicitacao.url}")
+@FeignClient(name = "consultaProposta", url = "${consulta.solicitacao.url}")
 public interface ConsultaPropostaClient {
 
-    @PostMapping
+    @PostMapping("/api/solicitacao")
     public ConsultaPropostaResponse solicitacao(ConsultaPropostaRequest consultaPropostaRequest);
 }
