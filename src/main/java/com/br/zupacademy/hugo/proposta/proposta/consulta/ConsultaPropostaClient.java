40ea30bf44e822df14006abfbe6687d81f42f7af
hugovallada <valladahugo@gmail.com>
2021-05-07 10:02:31 -0300

    Adiciona url de consulta nos properties

--- src/main/java/com/br/zupacademy/hugo/proposta/proposta/consulta/ConsultaPropostaClient.java
+++ src/main/java/com/br/zupacademy/hugo/proposta/proposta/consulta/ConsultaPropostaClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "ConsultaProposta", url ="http://localhost:9999/" )
+@FeignClient(name = "ConsultaProposta", url = "${consulta.solicitacao.url}")
 public interface ConsultaPropostaClient {
 
-    @PostMapping("api/solicitacao")
+    @PostMapping
     public ConsultaPropostaResponse solicitacao(ConsultaPropostaRequest consultaPropostaRequest);
 }
