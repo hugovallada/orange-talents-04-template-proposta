834512744868b50c507bf02b7d6103568ac57668
hugovallada <valladahugo@gmail.com>
2021-05-13 11:45:09 -0300

    Cria classe de configuração de segurança durante o desenvolvimento, para burlar o problema de timezone do keycloak

---
+++ src/main/java/com/br/zupacademy/hugo/proposta/config/security/DevSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.br.zupacademy.hugo.proposta.config.security;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile("dev")
+public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/propostas").permitAll()
+                .antMatchers(HttpMethod.POST,"/cartoes/**").permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
